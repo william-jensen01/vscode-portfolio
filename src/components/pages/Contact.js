af303fc91469e0770dde64d43f6e06f5732339fd
William Jensen <williambjensen01@gmail.com>
2024-10-21 21:50:08 -0600

    refactored Contact page with editor system utilizing more of a component-based structure

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,74 +1,91 @@
-import Codebar from "../Codebar";
-
-import "../../styles/contact.css";
+import {
+	EditorInstance,
+	Comment,
+	OpenParen,
+	CloseParen,
+	Numerical,
+} from "../Editor/index";
+import { CSS } from "../Editor/CSS";
 
 function Contact() {
-  return (
-    <div className="contact-container">
-      <Codebar amount={8} type="ct" />
-      <div className="contact">
-        <p className="ct-css-selector">
-          <span className="ct-css-name">.contact-me </span>
-          <span className="ct-css-bracket">{`{`}</span>
-        </p>
-        <p className="ct-css-line">
-          email:{" "}
-          <span className="ct-css-value">williambjensen01@gmail.com</span>;
-        </p>
-        <p className="ct-css-line">
-          github:{" "}
-          <span>
-            <a
-              href="https://github.com/william-jensen01"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="ct-css-value"
-            >
-              https://github.com/william-jensen01
-            </a>
-          </span>
-          ;
-        </p>
-        <p className="ct-css-line">
-          linkedin:{" "}
-          <span>
-            <a
-              href="https://www.linkedin.com/in/williambjensen/"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="ct-css-value"
-            >
-              https://www.linkedin.com/in/williambjensen/
-            </a>
-          </span>
-          ;
-        </p>
-        <p className="ct-css-line">
-          twitter:{" "}
-          <span>
-            <a
-              href="https://twitter.com/wbjensen"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="ct-css-value"
-            >
-              https://twitter.com/wbjensen
-            </a>
-          </span>
-          ;
-        </p>
-        <p className="ct-css-line">
-          location:{" "}
-          <span className="ct-css-value">
-            <span className="ct-css-quote">"</span>Provo, UT
-            <span className="ct-css-quote">"</span>
-          </span>
-          ;
-        </p>
-        <p className="ct-css-bracket">{`}`}</p>
-      </div>
-    </div>
-  );
+	return (
+		<EditorInstance>
+			<CSS>
+				<CSS.Rule>
+					<CSS.Selector>contact-me</CSS.Selector>
+					<CSS.Declaration
+						property={"email"}
+						values={[["williambjensen01@gmail.com", "str"]]}
+					></CSS.Declaration>
+					<CSS.Declaration
+						property="github"
+						values={[["https://github.com/wiliam-jensen01", "url"]]}
+					>
+						<Comment>click to open</Comment>
+					</CSS.Declaration>
+					<CSS.Declaration
+						property="linkedIn"
+						values={[["https://www.linkedin.com/in/williambjensen/", "url"]]}
+					>
+						<Comment>click to open</Comment>
+					</CSS.Declaration>
+					<CSS.Declaration
+						property="location"
+						values={[["Saratoga Springs, Utah", "str"]]}
+					/>
+					<CSS.Declaration
+						property="preferred contact method"
+						values={[["email", "str"]]}
+					></CSS.Declaration>
+				</CSS.Rule>
+				<CSS.Rule>
+					<CSS.Selector>status</CSS.Selector>
+					<CSS.Declaration
+						property="current project"
+						values={[["updating portfolio", "str"]]}
+					/>
+					<CSS.Declaration
+						property="learning next"
+						values={[["GraphQL", "str"]]}
+					/>
+					<CSS.Declaration
+						property="open to opportunities"
+						values={[["true"]]}
+					/>
+				</CSS.Rule>
+				<CSS.Rule>
+					<CSS.Selector>fun</CSS.Selector>
+					<CSS.Declaration property="color" values={[["00ff00", "color"]]} />
+					<CSS.Declaration
+						property="font family"
+						values={[["Comic Sans", "str"], ["monospace"]]}
+						separator=", "
+					>
+						<Comment>Just kidding!</Comment>
+					</CSS.Declaration>
+					<CSS.Declaration
+						property="animation"
+						values={[["always-learning"], ["1s", "numerical"], ["infinite"]]}
+					/>
+					<CSS.Declaration
+						property="transform"
+						values={[
+							[
+								<span style={{ color: "var(--css-value)" }}>
+									rotate
+									<OpenParen />
+									<Numerical>90deg</Numerical>
+									<CloseParen />
+								</span>,
+							],
+						]}
+					>
+						<Comment>I can see things from a different perspective</Comment>
+					</CSS.Declaration>
+				</CSS.Rule>
+			</CSS>
+		</EditorInstance>
+	);
 }
 
 export default Contact;
