7c0d847a89fc8121543ca15174e0ba18ee18cbc5
William Jensen <williambjensen01@gmail.com>
2024-10-22 18:21:46 -0600

    simplified logic

--- src/components/ExplorerItemList.js
+++ src/components/ExplorerItemList.js
@@ -6,46 +6,42 @@ import { ActiveFileContext } from "../Context/ActiveFileContext";
 import "../styles/explorer.css";
 
 function ExplorerItemList({ listItems }) {
-  const { taskbarContent, setTaskbarContent } = useContext(TaskbarContext);
+	const { taskbarContent, setTaskbarContent } = useContext(TaskbarContext);
 
-  const { activeFile, setActiveFile } = useContext(ActiveFileContext);
+	const { activeFile, setActiveFile } = useContext(ActiveFileContext);
 
-  const handleClick = (item) => {
-    // if item is in taskbar, set active file to it and highlight item in taskbar
-    let temp = null;
-    for (let i = 0; i < taskbarContent.length; i++) {
-      if (taskbarContent[i].fileName === item.fileName) {
-        temp = 1;
-        setActiveFile(item);
-      }
-    }
+	const handleClick = (item) => {
+		const taskbarItem = taskbarContent.find(
+			(taskItem) => taskItem.fileName === item.fileName
+		);
 
-    // if item not in taskbar, add it and set it as the active file
-    if (!temp) {
-      setTaskbarContent([...taskbarContent, item]);
-      setActiveFile(item);
-    }
-  };
-  return (
-    <div className="exp-item-content">
-      {listItems.map((item) => (
-        <div
-          key={item.fileName}
-          className={
-            item.fileName === activeFile.fileName
-              ? "exp-list-item-active"
-              : "exp-list-item"
-          }
-          onClick={() => handleClick(item)}
-        >
-          <svg className="exp-file-type">
-            <use href={sprite + `#${item.svg}`} />
-          </svg>
-          <p>{item.fileName}</p>
-        </div>
-      ))}
-    </div>
-  );
+		// item not in taskbar: add to taskbar
+		// set to active file
+		if (!taskbarItem) {
+			setTaskbarContent([...taskbarContent, item]);
+		}
+		setActiveFile(item);
+	};
+	return (
+		<div className="exp-item-content">
+			{listItems.map((item) => (
+				<div
+					key={item.fileName}
+					className={
+						item.fileName === activeFile.fileName
+							? "exp-list-item-active"
+							: "exp-list-item"
+					}
+					onClick={() => handleClick(item)}
+				>
+					<svg className="exp-file-type">
+						<use href={sprite + `#${item.svg}`} />
+					</svg>
+					<p>{item.fileName}</p>
+				</div>
+			))}
+		</div>
+	);
 }
 
 export default ExplorerItemList;
