f37ae99b73ad4a68223cdc6f00505d02dbc033e8
William Jensen <williambjensen01@gmail.com>
2025-05-28 19:24:44 -0600

    Single source of data for portfolio content, i.e. different pages

--- src/App.js
+++ src/App.js
@@ -12,60 +12,73 @@ import { ActiveFileContext } from "./Context/ActiveFileContext";
 import "./styles/layout.css";
 import "./styles/themes.css";
 
+export const portfolioContent = [
+	{
+		svg: "icon-markdown",
+		fileName: "about.md",
+		page: "about",
+	},
+	{
+		svg: "icon-js",
+		fileName: "skills.js",
+		page: "skills",
+	},
+	{
+		svg: "icon-html",
+		fileName: "projects.html",
+		page: "projects",
+	},
+	{
+		svg: "icon-css",
+		fileName: "contact.css",
+		page: "contact",
+	},
+];
+
 function App() {
-  const [taskbarContent, setTaskbarContent] = useState([
-    {
-      svg: "icon-markdown",
-      fileName: "about.md",
-      page: "about",
-    },
-  ]);
+	const [taskbarContent, setTaskbarContent] = useState(portfolioContent);
 
-  const [activeFile, setActiveFile] = useState({
-    svg: "icon-markdown",
-    fileName: "about.md",
-    page: "about",
-  });
+	const [activeFile, setActiveFile] = useState(portfolioContent[0]);
 
-  const providerTaskbar = useMemo(
-    () => ({ taskbarContent, setTaskbarContent }),
-    [taskbarContent]
-  );
+	const providerTaskbar = useMemo(
+		() => ({ taskbarContent, setTaskbarContent }),
+		[taskbarContent]
+	);
 
-  const providerActiveFile = useMemo(
-    () => ({ activeFile, setActiveFile }),
-    [activeFile]
-  );
+	const providerActiveFile = useMemo(
+		() => ({ activeFile, setActiveFile }),
+		[activeFile]
+	);
 
-  const keyMap = {
-    change_theme: "w",
-  };
-  const handlers = {
-    change_theme: () => console.log("change theme hotkey"),
-  };
+	const keyMap = {
+		change_theme: "w",
+	};
+	const handlers = {
+		change_theme: () => console.log("change theme hotkey"),
+	};
 
-  return (
-    <div className="layout">
-      <HotKeys keyMap={keyMap} handlers={handlers}>
-        <ActiveFileContext.Provider value={providerActiveFile}>
-          <div className="top-container">
-            <Header />
-          </div>
-          <TaskbarContext.Provider value={providerTaskbar}>
-            <div className="main">
-              <div className="left-container">
-                <Sidebar />
-              </div>
-              <div className="main-container">
-                <Taskbar />
-                <File />
-              </div>
-            </div>
-          </TaskbarContext.Provider>
-        </ActiveFileContext.Provider>
-      </HotKeys>
-    </div>
-  );
+	return (
+		<div className="layout">
+			<HotKeys keyMap={keyMap} handlers={handlers}>
+				<ActiveFileContext.Provider value={providerActiveFile}>
+					<div className="top-container">
+						<Header />
+					</div>
+					<TaskbarContext.Provider value={providerTaskbar}>
+						<div className="main">
+							<div className="left-container">
+								<Sidebar />
+							</div>
+							<div className="main-container">
+								<Taskbar />
+								<File />
+							</div>
+						</div>
+					</TaskbarContext.Provider>
+				</ActiveFileContext.Provider>
+			</HotKeys>
+		</div>
+	);
 }
 
 export default App;
