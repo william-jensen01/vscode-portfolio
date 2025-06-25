535f927166c19aee92226df32cc6cbe98f092c6f
William Jensen <williambjensen01@gmail.com>
2025-06-25 00:51:44 -0600

    feat: enhance settingsStore with new functionality
    - Implemented `getAllSettings` method to retrieve all valid settings as an object.
    - Introduced `groupByHierarchy` method for organizing settings based on a specified hierarchy.

--- src/store/settingsStore.js
+++ src/store/settingsStore.js
@@ -1,7 +1,6 @@
 import { create } from "zustand";
 import { devtools } from "zustand/middleware";
 import Logger from "../util/logger";
-
 const logger = new Logger("settingsStore");
 
 const INIT_VALUE = {
@@ -43,6 +42,18 @@ const settingsStore = create(
 			...INIT_VALUE,
 			changeSpecificSetting: (key, value) => {
 				logger.suffix("changeSpecificSetting").log(key, value);
+
+				// Special case for theme: set attribute on document
+				if (key === "theme") {
+					document?.documentElement?.setAttribute(
+						"data-theme",
+						value.value
+					);
+				}
+
+				// Do we let value be the index of new option?
+				// value: state[key].options[value]
+
 				set((state) => ({
 					...state,
 					[key]: {
@@ -50,6 +61,46 @@ const settingsStore = create(
 						value: value,
 					},
 				}));
+			},
+
+			getAllSettings: () => {
+				const all = { ...get() };
+				Object.keys(all).forEach((key) => {
+					if (typeof all[key] !== "object") delete all[key];
+				});
+				return all;
+			},
+
+			groupByHierarchy: () => {
+				logger.suffix("groupByHierarchy");
+				const addToGroup = (groups, keys, depth, key, obj) => {
+					if (depth >= keys.length) {
+						return;
+					}
+
+					const groupKey = obj[keys[depth]];
+
+					if (!groups[groupKey]) {
+						groups[groupKey] = {};
+					}
+
+					if (depth === keys.length - 1) {
+						groups[groupKey][key] = obj;
+					} else {
+						addToGroup(groups[groupKey], keys, depth + 1, key, obj);
+					}
+				};
+
+				// const hierarchyKeys = ["category", "sub_category"];
+				const hierarchyKeys = ["category"];
+				const result = {};
+				const settings = get().getAllSettings();
+
+				Object.entries(settings).forEach(([key, obj]) => {
+					addToGroup(result, hierarchyKeys, 0, key, obj);
+				});
+
+				return result;
 			},
 		};
 	})
