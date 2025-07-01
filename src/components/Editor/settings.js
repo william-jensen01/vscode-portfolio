367d2390e3de41e4ad7fc2881174b2a1566c69c0
William Jensen <williambjensen01@gmail.com>
2025-07-01 16:52:06 -0600

    feat(settings-indentation-guide): add indentation guides setting and update styles
    - Introduced a new setting for indentation guides in `Editor/settings.js` to control the rendering of indent guides in the editor.
    - Updated CSS to manage the visibility of these guides based on setting.

--- src/components/Editor/settings.js
+++ src/components/Editor/settings.js
@@ -1,7 +1,21 @@
 import lineCountSettings from "./Features/LineCount/settings";
 import bracketPairColorizationSettings from "./Features/BracketPairColorization/settings";
 
+const editorSettings = [
+	{
+		key: "indentationGuides",
+		default: true,
+		value: true, // true | false
+		input: "checkbox",
+		description: "Controls whether the editor should render indent guides.",
+		navigation: "editor.guides.indentation",
+		data_attribute: "indentation-guides",
+		category: "editor",
+	},
+];
+
 export const allEditorSettings = [
+	...editorSettings,
 	...lineCountSettings,
 	...bracketPairColorizationSettings,
 ].reduce((acc, setting) => {
