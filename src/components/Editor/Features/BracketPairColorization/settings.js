ddc775d71124de369eba829a84f3bedcf471bb3f
William Jensen <williambjensen01@gmail.com>
2025-06-30 20:38:18 -0600

    feat(bracket-colorization-settings): add settings and styles for bracket pair colorization
    - Introduced new settings for bracket pair colorization in `BracketPairColorization/settings.js`.
    - Updated `allEditorSettings` to include bracket pair colorization settings.
    - Enhanced CSS to support the colorization of brackets based on user settings.

---
+++ src/components/Editor/Features/BracketPairColorization/settings.js
@@ -0,0 +1,13 @@
+export default [
+	{
+		key: "bracketPairColorization.enabled",
+		default: true,
+		value: true, // true | false
+		input: "checkbox",
+		description:
+			"Controls whether bracket pair colorization is enabled or not.",
+		navigation: "editor.bracketPairColorization.enabled",
+		data_attribute: "bracket-pair-colorization",
+		category: "editor",
+	},
+];
--- src/components/Editor/settings.js
+++ src/components/Editor/settings.js
@@ -1,11 +1,12 @@
 import lineCountSettings from "./Features/LineCount/settings";
+import bracketPairColorizationSettings from "./Features/BracketPairColorization/settings";
 
-export const allEditorSettings = [...lineCountSettings].reduce(
-	(acc, setting) => {
-		return {
-			...acc,
-			[setting.key]: setting,
-		};
-	},
-	{}
-);
+export const allEditorSettings = [
+	...lineCountSettings,
+	...bracketPairColorizationSettings,
+].reduce((acc, setting) => {
+	return {
+		...acc,
+		[setting.key]: setting,
+	};
+}, {});
